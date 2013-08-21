ae91f11886619a4afb3e6f718ea39a03105f6d1f
Joshua Moody <joshuajmoody@gmail.com>
2013-08-21 14:21:39 +0200

    installed calabash and reproduced the problem

---
+++ SwipeableExample/console_ios6.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+./install_calabash_gem.sh
+
+TMP_DEVICE="iphone"
+
+if [ ! -z $1 ]; then
+  TMP_DEVICE=$1
+fi
+
+DEVICE=$TMP_DEVICE \
+CALABASH_FULL_CONSOLE_OUTPUT=1 \
+DEVICE_TARGET=simulator \
+OS=ios6 \
+IRBRC=.irbrc irb
